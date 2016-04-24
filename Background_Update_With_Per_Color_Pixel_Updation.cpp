ea1d819e35301f14a254a6a7b5e70a6fcb3a6495
DeepakDawar <ddeighty5@gmail.com>
2016-04-24 15:56:32 -0500

    change 1

--- Background_Update_With_Per_Color_Pixel_Updation.cpp
+++ Background_Update_With_Per_Color_Pixel_Updation.cpp
@@ -67,9 +67,14 @@ int main(int argc, char** argv)
 	
 	while (1) {
 		frame_no++;
+		
 		//new frame
 		cap >> nextframe;
 		if (nextframe.empty()) break;
+		if (frame_no < 2000){
+			background = nextframe.clone();
+			continue;
+		}
 		
 		//get region of interest
 		if (ROI_ENABLED){
@@ -249,7 +254,7 @@ int main(int argc, char** argv)
 								cout << "Vehicle removed from memory" << endl;
 								--vehicles;
 								cout << "Vehicle left in memory: " << vehicles << endl;
-								pause();
+								//pause();
 							}
 						}
 						else if (vehicles_detected[i][0] >= pt1.x){
@@ -263,7 +268,7 @@ int main(int argc, char** argv)
 								cout << "Vehicle removed from memory" << endl;
 								--vehicles;
 								cout << "Vehicle left in memory: " << vehicles << endl;
-								pause();
+								//pause();
 							}
 						}
 						//update the x-coordinate
@@ -340,7 +345,7 @@ int main(int argc, char** argv)
 			}
 			
 		}//end of for involving contour check
-		if (frame_no > 10000)
+		if (frame_no % 2000==0)
 			pause();
 		bool stuck = false;
 		/***************check for stuck contour******************/
@@ -397,7 +402,7 @@ int main(int argc, char** argv)
 		Mat new_background = background.clone();
 		Scalar value;
 		int pixels_updated = 0;
-		if (!copy_frame_as_background && !stuck){
+		if ((!copy_frame_as_background && !stuck) || vehicle_width > frame_width/2){
 			for (int i = 0; i < background.cols; i++){
 				for (int j = 0; j < background.rows; j++){
 					//int intensity_diff = abs((int)gray_background.at<uchar>(Point(i, j)) - (int)gray_nextframe.at<uchar>(Point(i, j)));
